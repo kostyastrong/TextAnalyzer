362171e4596ecc4d6f60faefb16193528bc72247
kostykostya872@gmail.com <kostyakostya872@gmail.com>
2021-12-08 19:30:58 +0300

    Working on Parsing CEFR

--- parseDictionaries/cambridgeCEFRParser.py
+++ parseDictionaries/cambridgeCEFRParser.py
@@ -1,10 +1,54 @@
 from bs4 import BeautifulSoup
 import requests
+from random import randint
 
-tmpUrl = 'https://www.englishprofile.org/british-english/words/detail/2495'
-r = requests.get(tmpUrl)
-print(r.text)  # check the 193rd line
+useless = dict({
+    'Back to Report':1,
+    'Copyright 2015 Cambridge University Press': 1,
+    'Terms of Use': 1,
+    'Back to Top': 1,
+    'English Profile - Words' : 1,
+    'Full View': 1
+})
+
+def wordInfoGetText(url):
+    r = requests.get(url)
+    bp = BeautifulSoup(r.text, 'html.parser')
+
+    response = bp.text
+
+    for i, j in useless.items():
+        response = response.replace(i, '', j)
+
+    response = response.strip()
+
+    print(response)
+    print(bp.find_all("span", {"class": "pos"}))
+    print()
+
+    # why BeautifulSoup get text doesn't work? We erase information about labels and codes
+    # there is a chance that I will get T in 'get' as a description or vise versa
+
+#wordInfoGetText('https://www.englishprofile.org/british-english/words/detail/2495')  # get
+#wordInfoGetText('https://www.englishprofile.org/british-english/words/detail/950')  # clear
 
 
-bp = BeautifulSoup(r.text, 'html.parser')
-for i in bp.find_all('')
+categ = set()
+
+def wordDescription(url):
+    global categ
+    r = requests.get(url)
+    bp = BeautifulSoup(r.text, 'html.parser')
+    content = bp.find_all('div', {'class': "evp_details"})[1]
+    partOfSpeech = None
+    for current in content.div.find_all(recursive=False):
+        if current['class'][0] == 'pos_header':  # new part of speech
+        categ.add(current['class'][0])
+
+
+wordDescription('https://www.englishprofile.org/british-english/words/detail/2495')  # get
+
+
+#wordDescription('https://www.englishprofile.org/british-english/words/detail/2495')  # get
+
+
---
+++ parseDictionaries/draft.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+from random import randint
+
+s = "textdsomethinguselssdagaindverb"
+s1 = "ab ba b"
+def find_all(s: str, need: str = ''):
+    j = 0
+    while True:
+        i = s.find(need, j + 1)
+        if i == -1:
+            yield -1
+            return
+        else:
+            yield i
+            j = i
+
+print(list(find_all(s, 'd')))
+print()
+print(list(find_all(s1, 'd')))
+print(s.split('d'))
+
+
+categ = set()
+
+def wordDescription(url):
+    global categ
+    r = requests.get(url)
+    bp = BeautifulSoup(r.text, 'html.parser')
+    content = bp.find_all('div', {'class': "evp_details"})[1]
+    for current in content.div.find_all(recursive=False):
+        categ.add(current['class'][0])
+
+
+wordDescription('https://www.englishprofile.org/british-english/words/detail/2495')  # get
+
+for i in range(100):
+    num = randint(1, 1000)
+    wordDescription('https://www.englishprofile.org/british-english/words/detail/' + str(num))
+
+print(categ)  # {'wordfam', 'info', 'pos_header'}
