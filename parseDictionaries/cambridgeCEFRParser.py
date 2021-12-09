de920f0f7fa1f529742de999ca2455a6c9673636
kostykostya872@gmail.com <kostyakostya872@gmail.com>
2021-12-09 18:26:27 +0300

    getting pos_section

--- parseDictionaries/cambridgeCEFRParser.py
+++ parseDictionaries/cambridgeCEFRParser.py
@@ -29,25 +29,77 @@ def wordInfoGetText(url):
     # why BeautifulSoup get text doesn't work? We erase information about labels and codes
     # there is a chance that I will get T in 'get' as a description or vise versa
 
-#wordInfoGetText('https://www.englishprofile.org/british-english/words/detail/2495')  # get
-#wordInfoGetText('https://www.englishprofile.org/british-english/words/detail/950')  # clear
+# wordInfoGetText('https://www.englishprofile.org/british-english/words/detail/2495')  # get
+# wordInfoGetText('https://www.englishprofile.org/british-english/words/detail/950')  # clear
 
+def clearWord(s: str):
+    s = s.strip()
+    start = 0
+    finish = len(s) - 1
+    while start < len(s) and not s[start].isalpha():
+        start += 1
+    while finish > start and not s[finish].isalpha():
+        finish -= 1
+    return s[start:finish + 1]
 
+def clearPartOfSpeech(s: str):
+    s = clearWord(s)
+    s = s.lower()
+    if s[-1] == 's':
+        s = s[:-1]
+    return s
+
+def getLabel(block, found = False):
+    label = None
+    try:
+        if found:
+            label = block.get_text()
+        else:
+            label = block.find('span', {'class': 'posgram'}).get_text()
+    except:
+        pass
+    return label
 
 def wordDescription(url):
+    ret = dict()
+
     r = requests.get(url)
     bp = BeautifulSoup(r.text, 'html.parser')
     content = bp.find_all('div', {'class': "evp_details"})[1]
+
     partOfSpeech = None
+    transcription = None
+    genLabel = None
+    family = None
     word = content.find('span', {'class': 'headword'}).get_text()
+
     print(word)
     # content.div.find_all(recursive=False) iterate through childs
-    for current in content:
-        #print(current['class'][0])
-        if current['class'][0] == 'pos_section':  # new part of speech
+    i = 0
+    for tmp in content:
+        print(i)
+        i+=1
+        current = BeautifulSoup(str(tmp),"html.parser")
+        if not current.find('div', {'class': 'pos_section'}) is None:
+            print('abobus')
             partOfSpeech = current.find('span', {'class': 'pos'}).get_text()
-            print(partOfSpeech)
-    print()
+            transcription = current.find('span', {'class' : 'written'}).get_text()
+            genLabel = getLabel(current)
+
+            wordsFam = current.find('div', {'class': 'wordfam'})
+            if not wordsFam is None and family is None:
+                family = dict()
+                for group in wordsFam.find_all('div', {'class' : 'wf_pos_block'}):
+                    part = clearWord(group.find('span', {'class': 'wf_pos'}).get_text())
+                    part = clearPartOfSpeech(part)
+                    examples = list(clearWord(i.get_text()) for i in group.find_all('span', {'class': 'wf_word'}))
+                    for j in examples:
+                        family[j] = [word, part]
+
+            for instance in current.find_all('div', {'class' : 'info sense'}):
+                short = instance.find('div', {'class': 'sense_title'}).get_text()
+                print(short)
+    return [ret, family]
 
 
 
