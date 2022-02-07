308b41a887d3d3dcb40b9a99eb81e982aca03ac1
kostykostya872@gmail.com <kostyakostya872@gmail.com>
2022-02-07 09:02:11 +0300

    js title

--- site/static/js/mainPage.js
+++ site/static/js/mainPage.js
@@ -2,5 +2,20 @@ var divs = document.getElementsByClassName('book-title');
 const title_lim = 18;
 for (var i = 0; i < divs.length; ++i) {
     if (divs[i].innerHTML.length < title_lim) continue;
-    divs[i].innerHTML ='...';  // we may use here href=
+    divs[i].innerHTML = divs[i].innerHTML.substring(0, title_lim) + '...';  // we may use here href=
+}
+
+var divs = document.getElementsByClassName('book-title');
+const title_lim = 18;
+for (var i = 0; i < divs.length; ++i) {
+    if (divs[i].innerHTML.length < title_lim) continue;
+    divs[i].innerHTML = divs[i].innerHTML.substring(0, title_lim) + '...';  // we may use here href=
+}
+
+function shorterByTag(tag, lim) {
+    var divs = document.getElementsByClassName(tag);
+    for (var i = 0; i < divs.length; ++i) {
+        if (divs[i].innerHTML.length < lim) continue;
+        divs[i].innerHTML = divs[i].innerHTML.substring(0, lim) + '...';  // we may use here href=
+    }
 }
