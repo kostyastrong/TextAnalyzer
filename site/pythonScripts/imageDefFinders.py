8a1f52f33b82d475664bd1e250c882407c3c7a51
kostykostya872@gmail.com <kostyakostya872@gmail.com>
2022-02-22 15:30:03 +0300

    trying to avoid ads

--- site/pythonScripts/imageDefFinders.py
+++ site/pythonScripts/imageDefFinders.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
+import mechanicalsoup
 
 
 def unsplashTenPhLinks(word: str, num=1, adblock=True):
@@ -14,16 +15,33 @@ def unsplashTenPhLinks(word: str, num=1, adblock=True):
     if not adblock:
         links = [img['src'] for img in soup.findAll('img') if not img['src'].find(enter) is None]
     else:
+        browser = mechanicalsoup.Browser()
+        page = browser.get(url)
+        soup = page.soup
+        j = 0
+        print(soup.find('a', string='Sponsored'))
+        with open('allwinter.txt', 'w') as f:
+            f.write(str(soup))
+
         for cont in soup.findAll('figure', {'itemprop': 'image'}):
-            if not (cont.find('a', string="Sponsored") is None): continue
-            links += [img['src'] for img in cont.findAll('img') if not img['src'].find(enter) is None]
+            if j == num: break
+            print(cont.find('a', string="Sponsored"))
+            if cont.find('a', string="Sponsored") is None:
+                links += [img['src'] for img in cont.findAll('img') if not img['src'].find(enter) is None]
+                print(cont.find('a', string='Sponsored'))
+                with open('winter.txt', 'w') as f:
+                    f.write(str(cont))
+                    print(links)
+                    break
+                j += 1
         #cont = [BeautifulSoup(i.text) for i in soup.findAll('figure', {'itemprop': 'image'}) if i.find('a', string='Sponsored') is None]
         #print(type(BeautifulSoup(cont[0].text)), type(soup))
         #links = [i.find(enter)['src'] for i in soup.findAll('figure', {'itemprop': 'image'}) if i.find('a', string='Sponsored') is None]
 
     return links[:num]
 
-print(unsplashTenPhLinks('coat'))
+#print(unsplashTenPhLinks('coat'))
 
+unsplashTenPhLinks('winter')
 for i in range(5):
-    print(unsplashTenPhLinks(input()))
+    print(unsplashTenPhLinks(input(), 2))
