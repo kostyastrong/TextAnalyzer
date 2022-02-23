7aba790d7afcfbe451a3255f409a3d4b54f41b9c
kostykostya872@gmail.com <kostyakostya872@gmail.com>
2022-02-23 12:50:47 +0300

    class for images for a word

--- site/pythonScripts/imageDefFinders.py
+++ site/pythonScripts/imageDefFinders.py
@@ -4,6 +4,74 @@ import requests
 import mechanicalsoup
 import random
 
+class picForWord():
+
+    @staticmethod
+    def gethtml(url):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+            "Upgrade-Insecure-Requests": "1", "DNT": "1",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+
+        r = requests.get(url, headers=headers)
+        return r.text
+
+    '''seems like there are no commercial pictures among 2-4 pictures, but we only need one of them'''
+    def unsplashOnePicture(self):
+        word = self.word
+        url = 'https://unsplash.com/s/photos/' + word
+        enter = 'https://images.unsplash.com/'
+
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        pic = [img['src'] for img in soup.findAll('img') if
+               img['src'].find(enter) != -1 and not img['src'].find(enter) is None]
+        return pic[random.randint(1, 3)]
+
+    '''Unlike Oxford, cambridge shows a lot of pictures on its page: 
+        different meaning of words (search for "Box") or quizzes and logos'''
+
+    def cambridgePic(self):
+        word = self.word
+        url = 'https://dictionary.cambridge.org/dictionary/english/' + word
+        soup = BeautifulSoup(picForWord.gethtml(url), features="html.parser")
+
+        out = soup.find('div', {'class': 'dimg'})
+        if out is None:
+            return [None, None]
+
+        smallPic = 'https://dictionary.cambridge.org' + out.find('amp-img')['src']
+        bigPic = 'https://dictionary.cambridge.org' + out.find('amp-img')['src'].replace('thumb', 'full', 1)
+        return [smallPic, bigPic]
+
+    def oxfordPic(self):
+        word = self.word
+        url = f'https://www.oxfordlearnersdictionaries.com/definition/english/{word}_1?q={word}'
+        soup = BeautifulSoup(picForWord.gethtml(url), features="html.parser")
+        out = soup.find('img', {'class': 'thumb'})
+
+        if out is None:
+            return [None, None]
+
+        smallPic = out['src']
+        bigPic = smallPic.replace('thumb', 'fullsize', 1)
+        return [smallPic, bigPic]
+
+    def __init__(self, word: str):
+        self.word = word
+
+        '''a link'''
+        self.pic = self.cambridgePic()[1]
+
+        if self.pic is None:
+            self.pic = self.oxfordPic()[1]
+
+        if self.pic is None:
+            self.pic = self.unsplashOnePicture()
+
+    def getPic(self):
+        return self.pic
 
 '''check the winter.txt and the allwinter.txt files with "Supported" marks. 
     These marks are in the end of the html page, so it's difficult to get whether photo is commercial or normal.'''
@@ -43,59 +111,6 @@ def unsplashTenPhLinks(word: str, num=1, adblock=True):  # it was supposed to be
 
     return links[:num]
 
-#print(unsplashTenPhLinks('coat'))
 
-#unsplashTenPhLinks('winter')
-# for i in range(5):
-#     print(unsplashTenPhLinks(input(), 2))
-
-
-'''seems like there are no commercial pictures among 2-4 pictures, but we only need one of them'''
-def unsplashOnePicture(word: str):
-    url = 'https://unsplash.com/s/photos/' + word
-    enter = 'https://images.unsplash.com/'
-
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    pic = [img['src'] for img in soup.findAll('img') if img['src'].find(enter) != -1 and not img['src'].find(enter) is None]
-    return pic[random.randint(1, 3)]
-
-
-'''Unlike Oxford, cambridge shows a lot of pictures on its page: 
-    different meaning of words (search for "Box") or quizzes and logos'''
-def cambridgePic(word: str):
-    url = 'https://dictionary.cambridge.org/dictionary/english/' + word
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-        "Upgrade-Insecure-Requests": "1", "DNT": "1",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
-
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    out = soup.find('div', {'class' : 'dimg'})
-    if out is None:
-        return None
-
-    smallPic = 'https://dictionary.cambridge.org' + out.find('amp-img')['src']
-    bigPic = 'https://dictionary.cambridge.org' + out.find('amp-img')['src'].replace('thumb', 'full', 1)
-    return [smallPic, bigPic]
-
-def oxfordPic(word: str):
-    url = f'https://www.oxfordlearnersdictionaries.com/definition/english/{word}_1?q={word}'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-        "Upgrade-Insecure-Requests": "1", "DNT": "1",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
-
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    smallPic = soup.find('img', {'class': 'thumb'})['src']
-    bigPic = smallPic.replace('thumb', 'fullsize', 1)
-    return [smallPic, bigPic]
-
-#print(unsplashOnePicture(input('Word: ')))
-#print(unsplashOnePicture('california'))
-#print(cambridgePic('coat'))
-#print(oxfordPic('box'))
+coat = picForWord('adidas')
+print(coat.pic)
